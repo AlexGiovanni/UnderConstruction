fc6043d57a7b075a80b34c1e154c3a4ae43a9f0e
Alexis <alexis.santiagoags@gmail.com>
2023-06-30 16:50:26 -0500

    :)

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
       <img src="https://images.ctfassets.net/2y61g8ovq8b2/3UnXBAYwsDwqAUTfuPLEIk/15ee542c84d4e74c610badce8e45252c/Logo.svg" alt="Girl in a jacket" width="300" height="400">
       <h1>Coming soon</h1>
       <p>Welcome to Bravery Unbound</p>
-      <p>This site is currently under construction</p>
+      <p>This site is currently under construction.</p>
     </div>
   </div>
 </template>
